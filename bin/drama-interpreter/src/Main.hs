ba663f7fd91371bf31eb251d7cd567e3c23ddf6f
Edward Jones <edwards.jones.94@gmail.com>
2015-12-15 20:19:33 +0000

    Fixes Main.hs.

--- bin/drama-interpreter/src/Main.hs
+++ bin/drama-interpreter/src/Main.hs
@@ -5,8 +5,9 @@ import qualified Tokens as L
 import qualified Happy as P
 import qualified Interpreter as I
 
+main :: IO ()
 main = do
     s <- getArgs
     fileContents <- readFile $ head s
-    ast <- P.parseDrama $ L.alexScanTokens fileContents
-    I.evalProgram ast
+    let ast = P.parseDrama $ L.alexScanTokens fileContents
+    print (I.evalProgram ast)
