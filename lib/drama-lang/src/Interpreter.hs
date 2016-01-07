f7007d12f3d01e784a60daa1b30c32bf723e2620
Edward Jones <edwards.jones.94@gmail.com>
2016-01-07 20:16:17 +0000

    Implements stepping compiler (MonadStepped, Stepped, SteppedResult etc. introduced)

--- lib/drama-lang/src/Interpreter.hs
+++ lib/drama-lang/src/Interpreter.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
 module Interpreter where
 
@@ -62,6 +66,80 @@ $(makeLenses ''GlobalEnv)
 $(makeLenses ''LocalEnv)
 $(makeLenses ''ActorInstance)
 
+data Stepped s a
+    = Stepped (s -> (SteppedResult s a, s))
+
+data SteppedResult s a
+    = Done a
+    | Suspended (Stepped s a)
+
+runStepped :: Stepped s a -> s -> (SteppedResult s a, s)
+runStepped (Stepped f) s
+    = f s
+
+instance Functor (Stepped s) where
+    fmap f (Stepped g)
+        = Stepped $
+            \s ->   let (r, s') = g s
+                    in  (fmap f r, s')
+
+instance Functor (SteppedResult s) where
+  --fmap takes an a -> b, and a structure, and applies that function
+    fmap f (Done a)
+        = Done (f a)
+
+    fmap f (Suspended k)
+        = Suspended (fmap f k)
+
+instance Applicative (Stepped s) where
+    pure
+        = return
+
+    sf <*> sx
+        = do
+            f <- sf
+            x <- sx
+            return (f x)
+
+instance Monad (Stepped s) where
+    
+    return x
+        = Stepped $
+            \s -> (Done x, s)
+
+--(>>=) :: Stepped s a -> (a -> Stepped s b) -> Stepped s b
+    ma >>= kamb
+        = Stepped $
+
+            \s -> case runStepped ma s of
+                (Done x, s') -> runStepped (kamb x) s'
+                (Suspended sa, s') -> (Suspended (sa >>= kamb), s')    
+
+instance MonadState s (Stepped s) where
+    get 
+        = Stepped $ \s -> (Done s, s)
+
+    put newState
+        = Stepped $ \_ -> (Done (), newState)
+
+class MonadState s m => MonadStepped s m where
+    defineStep :: m a -> m a
+
+instance Monad m => MonadStepped s (StateT s m) where
+    defineStep 
+        = id
+
+instance MonadStepped s (Stepped s) where
+  --defineStep :: Stepped s a -> Stepped s a
+    defineStep m
+        = Stepped $ \s -> (Suspended m, s)
+
+step :: Stepped s a -> s -> (Maybe (Stepped s a), s)
+step (Stepped f) s
+    = case f s of
+        (Done x, s') -> (Nothing, s')
+        (Suspended k, s') -> (Just k, s')
+    
 isLocalEnv :: Lens' IState LocalEnv
 isLocalEnv f is
     = fmap k (f lenv)
@@ -92,17 +170,18 @@ evalProgram (Program bs inst)
 --neads to evaluate either a message, or prereceive
 --will then call schedule again with new genv
 --works out next actor by getting ids, getting ready ones, and then picking one
-scheduler :: State IState ()
+--scheduler :: State IState ()
+scheduler :: MonadStepped IState m => m ()
 scheduler
     = do 
         is <- get
-        let genv = trace ("Scheduling, current genv is " ++ (show (_isGlobalEnv is)) ++ "\n") _isGlobalEnv is
+        let genv = _isGlobalEnv is
             allIds = M.keys (_geActorInstances genv)
         ready <- getReady allIds
         if null ready 
             then return ()
             else do 
-                let toBeEval = trace ("Ready actor is " ++ (show (head ready)) ++ "\n") head ready
+                let toBeEval = head ready
                 actor <- lookupActorInstance toBeEval
                 let readyToReceive = _aiCanReceive actor
                     actorId = _aiId actor
@@ -121,50 +200,54 @@ scheduler
                         isGlobalEnv . geActorInstances %= M.update (replaceActor newActor) actorId
                         scheduler
 
-preReceive :: ActorId -> PreReceive -> State IState Value
+--preReceive :: ActorId -> PreReceive -> State IState Value
+preReceive :: MonadStepped IState m => ActorId -> PreReceive -> m Value
 preReceive aid pr 
-    = do
-        v <- evalExp aid pr
-        actor <- lookupActorInstance aid
-        let newActor = actor { _aiCanReceive = True }
-        isGlobalEnv . geActorInstances %= M.update (replaceActor newActor) (_aiId actor) 
-        return v
+    = defineStep $ 
+        do
+            v <- evalExp aid pr
+            actor <- lookupActorInstance aid
+            let newActor = actor { _aiCanReceive = True }
+            isGlobalEnv . geActorInstances %= M.update (replaceActor newActor) (_aiId actor) 
+            return v
 
 replaceActor :: ActorInstance -> ActorInstance -> Maybe ActorInstance
 replaceActor newActor oldActor = if oldActor == oldActor then Just newActor else Nothing
 
-receive :: ActorInstance -> [Receive] -> State IState Value
+--receive :: ActorInstance -> [Receive] -> State IState Value
+receive :: MonadStepped IState m => ActorInstance -> [Receive] -> m Value
 receive ai rec 
-    = do
-        -- get aps from msg, and take out that msg
-        let (aps : msgs) = trace ("Receive on actor " ++ (show ai)) _aiInbox ai
-            ai' = ai { _aiInbox = msgs }
+    = defineStep $
+        do
+            -- get aps from msg, and take out that msg
+            let (aps : msgs) = _aiInbox ai
+                ai' = ai { _aiInbox = msgs }
 
-        -- replace the actor with one which doesn't have that msg anymore
-        isGlobalEnv . geActorInstances %= M.update (replaceActor ai') (_aiId ai)
+            -- replace the actor with one which doesn't have that msg anymore
+            isGlobalEnv . geActorInstances %= M.update (replaceActor ai') (_aiId ai)
 
-        -- find which handler to use, and create new msg bindings
-        let (fps, handling) = matchArity rec aps
-            bindings = M.fromList (zip fps aps)
-        -- get env without msg bindings, for after
-        lenv <- use isLocalEnv
-        -- add msg bindings to lenv
-        isLocalEnv . leBindings %= M.union bindings
+            -- find which handler to use, and create new msg bindings
+            let (fps, handling) = matchArity rec aps
+                bindings = M.fromList (zip fps aps)
+            -- get env without msg bindings, for after
+            lenv <- use isLocalEnv
+            -- add msg bindings to lenv
+            isLocalEnv . leBindings %= M.union bindings
 
-        -- eval expression of matched msg handler
-        v <- evalExp (_aiId ai) handling 
-        
-        -- make sure to get rid of temporary msg bindings in actor's local env
-        -- put original bindings back
-        isLocalEnv . leBindings .= _leBindings lenv
-        -- get current lenv, minus temp bindings
-        lenv2 <- use isLocalEnv
-        -- actor becomes one after execution, but without msg bindings
-        let ai2 = ai' { _aiEnv = lenv2 }
-        -- replace actor with new one
-        isGlobalEnv . geActorInstances %= M.update (replaceActor ai2) (_aiId ai2)
+            -- eval expression of matched msg handler
+            v <- evalExp (_aiId ai) handling 
+            
+            -- make sure to get rid of temporary msg bindings in actor's local env
+            -- put original bindings back
+            isLocalEnv . leBindings .= _leBindings lenv
+            -- get current lenv, minus temp bindings
+            lenv2 <- use isLocalEnv
+            -- actor becomes one after execution, but without msg bindings
+            let ai2 = ai' { _aiEnv = lenv2 }
+            -- replace actor with new one
+            isGlobalEnv . geActorInstances %= M.update (replaceActor ai2) (_aiId ai2)
 
-        return v
+            return v
 
 matchArity :: [Receive] -> Message -> Receive
 matchArity [] _ = error "matchArity: no matching cases for message"
@@ -186,13 +269,15 @@ getNames :: [Behaviour] -> [Name]
 getNames [] = []
 getNames ((Behaviour name _ _ _) : bs) = name : getNames bs
 
-instantiate :: Instantiation -> State IState ActorId
+--instantiate :: Instantiation -> State IState ActorId
+instantiate :: MonadStepped IState m => Instantiation -> m ActorId
 instantiate (Instantiation name aps)
     = do
         ActorV firstId <- evalExp 0 (CreateE name aps) 
         return firstId
 
-getReady :: [ActorId] -> State IState [ActorId]
+--getReady :: [ActorId] -> State IState [ActorId]
+getReady :: MonadStepped IState m => [ActorId] -> m [ActorId]
 getReady [] = return []
 getReady (aid : aids)
     = do
@@ -201,7 +286,8 @@ getReady (aid : aids)
         readyAids <- getReady aids
         return (if ready then aid : readyAids else readyAids)
 
-evalExp :: ActorId -> Exp -> State IState Value
+--evalExp :: ActorId -> Exp -> State IState Value
+evalExp :: MonadStepped IState m => ActorId -> Exp -> m Value
 evalExp _ UnitE
     = return UnitV
 
@@ -211,52 +297,58 @@ evalExp aid SelfE
 evalExp _ (NumberE x) 
     = return (NumberV x)
 
+--gives an IState Value (\s -> var_val, s')
 evalExp _ (VarE name)
     = lookupName name
 
 evalExp selfId (SendE name aps)
-    = do
-        msg <- evalExps selfId aps
-        ActorV otherId <- lookupName name
-        otherInst <- lookupActorInstance otherId
-        let newInbox = (_aiInbox otherInst) ++ [msg]
-            otherInst' = otherInst { _aiInbox = newInbox }
-        isGlobalEnv . geActorInstances %= M.insert otherId otherInst'
-        return UnitV
+    = defineStep $
+        do
+            msg <- evalExps selfId aps
+            ActorV otherId <- lookupName name
+            otherInst <- lookupActorInstance otherId
+            let newInbox = (_aiInbox otherInst) ++ [msg]
+                otherInst' = otherInst { _aiInbox = newInbox }
+            isGlobalEnv . geActorInstances %= M.insert otherId otherInst'
+            return UnitV
 
 evalExp aid (LetE name exp1 exp2)
-    = do
-        v <- evalExp aid exp1
-        isLocalEnv . leBindings %= M.insert name v
-        evalExp aid exp2
+    = defineStep $
+        do
+            v <- evalExp aid exp1
+            isLocalEnv . leBindings %= M.insert name v
+            evalExp aid exp2
 
 evalExp aid (CreateE name aps)
-    = do 
-        behaviour@(Behaviour _ fps _ _) <- lookupBehaviour name
-        newId <- isGlobalEnv . geNextAvailableActor <<%= (+1)
-        vs <- evalExps aid aps 
-        let newLocalEnv 
-                = LocalEnv 
-                    { _leBindings = M.fromList (zip fps vs) 
-                    , _leConsole = []
-                    }
-            newActor 
-                = ActorInstance
-                    { _aiId = newId
-                    , _aiInbox = []
-                    , _aiBehaviour = behaviour
-                    , _aiEnv = newLocalEnv
-                    , _aiCanReceive = False
-                    }
-        isGlobalEnv . geActorInstances %= M.insert newId newActor
-        return (ActorV newId)
+    = defineStep $
+        do 
+            behaviour@(Behaviour _ fps _ _) <- lookupBehaviour name
+            newId <- isGlobalEnv . geNextAvailableActor <<%= (+1)
+            vs <- evalExps aid aps 
+            let newLocalEnv 
+                    = LocalEnv 
+                        { _leBindings = M.fromList (zip fps vs) 
+                        , _leConsole = []
+                        }
+                newActor 
+                    = ActorInstance
+                        { _aiId = newId
+                        , _aiInbox = []
+                        , _aiBehaviour = behaviour
+                        , _aiEnv = newLocalEnv
+                        , _aiCanReceive = False
+                        }
+            isGlobalEnv . geActorInstances %= M.insert newId newActor
+            return (ActorV newId)
 
 evalExp aid (PrintE s e)
-    = do
-        isLocalEnv . leConsole %= (++ [s])
-        evalExp aid e
+    = defineStep $
+        do
+            isLocalEnv . leConsole %= (++ [s])
+            evalExp aid e
 
-evalExps :: ActorId -> [Exp] -> State IState [Value]
+--evalExps :: ActorId -> [Exp] -> State IState [Value]
+evalExps :: MonadStepped IState m => ActorId -> [Exp] -> m [Value]
 evalExps _ [] 
     = return []
 
@@ -266,20 +358,23 @@ evalExps aid (e : es)
         vs <- evalExps aid es
         return (v : vs)
         
-lookupName :: Name -> State IState Value
+--lookupName :: Name -> State IState Value
+lookupName :: MonadStepped IState m => Name -> m Value
 lookupName name 
-    = do 
+    = do
         bindings <- use (isLocalEnv . leBindings)
         lenv <- use isLocalEnv
-        return (M.findWithDefault (error (show lenv)) name bindings)
+        return (M.findWithDefault (error "lookupName: invalid name") name bindings)
 
-lookupActorInstance :: ActorId -> State IState ActorInstance
+--lookupActorInstance :: ActorId -> State IState ActorInstance
+lookupActorInstance :: MonadStepped IState m => ActorId -> m ActorInstance
 lookupActorInstance aid 
     = do 
         actors <- use (isGlobalEnv . geActorInstances)
         return (M.findWithDefault (error "lookupActorInstance: invalid id") aid actors)
 
-lookupBehaviour :: Name -> State IState Behaviour
+--lookupBehaviour :: Name -> State IState Behaviour
+lookupBehaviour :: MonadStepped IState m => Name -> m Behaviour
 lookupBehaviour name 
     = do 
         behaviours <- use (isGlobalEnv . geBehaviours)
